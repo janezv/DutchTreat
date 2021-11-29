e23f293553ec0bfa05c1c4200a65103da1cb26c0
janezv <janez.vegan@gmail.com>
2021-11-29 21:41:34 +0100

    Add project files.

---
+++ Services/NullMailService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Services
+{
+    public class NullMailService : IMailService
+    {
+
+        public ILogger<NullMailService> _Logger;
+
+        public NullMailService(ILogger<NullMailService> logger)
+        {
+            _Logger = logger;
+        }
+
+
+        public void SendMessage(string to, string subject, string body)
+        {
+            _Logger.LogInformation($"To:{to}  Subject:{subject}  Body:{body}");
+        }
+    }
+}
